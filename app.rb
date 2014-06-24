33703a50da816cda8cdfefb33702f52bac8a155a
Andrew Stewart <andrew@stwrt.ca>
2014-06-23 22:10:16 -0700

    Initial commit

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'httmultiparty'
+
+require 'base64'
+
+IMGUR_API = "https://api.imgur.com/3/image"
+CLIENT_ID = ENV.fetch("IMGUR_CLIENT_ID")
+
+post '/' do
+  content_type :json
+
+  unless params[:media]
+    p params
+    return ""
+  end
+
+  file = File.new(params[:media][:tempfile])
+
+  response = HTTMultiParty.post(IMGUR_API, {
+    :headers => { "Authorization" => "Client-ID #{CLIENT_ID}" },
+    :body => { :image => file }
+  })
+
+  body = response.parsed_response
+
+  unless body["status"] == 200
+    p response
+    return ""
+  end
+
+  { :url => body["data"]["link"] }.to_json
+end
