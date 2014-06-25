5756c09718c90af40758a6c485863ca6625d7968
Andrew Stewart <andrew@stwrt.ca>
2014-06-24 18:58:25 -0700

    this technically counts as a refactor right

--- app.rb
+++ app.rb
@@ -1,30 +1,36 @@
 require 'sinatra'
 require 'httmultiparty'
 
-IMGUR_API = "https://api.imgur.com/3/image"
-CLIENT_ID = ENV.fetch "IMGUR_CLIENT_ID"
+class Imgur
+  ENDPOINT = 'https://api.imgur.com/3/image'
+  CLIENT_ID = ENV.fetch 'IMGUR_CLIENT_ID'
+
+  def self.upload(filename)
+    resp = HTTMultiParty.post(
+      ENDPOINT,
+      {
+        :headers => {
+          'Authorization' => 'Client-ID ' + CLIENT_ID
+        },
+
+        :body => {
+          :image => File.new(filename)
+        }
+      }
+    )
+
+    handle_response resp
+  end
+
+  def self.handle_response(response)
+    return '' unless response['status'] == 200
+    { :url => response['data']['link'] }
+  end
+end
 
 post '/' do
   content_type :json
-
-  unless params[:media]
-    p params
-    return ""
-  end
-
-  file = File.new(params[:media][:tempfile])
-
-  response = HTTMultiParty.post(IMGUR_API, {
-    :headers => { "Authorization" => "Client-ID #{CLIENT_ID}" },
-    :body => { :image => file }
-  })
-
-  body = response.parsed_response
-
-  unless body["status"] == 200
-    p response
-    return ""
-  end
-
-  { :url => body["data"]["link"] }.to_json
+  return '' unless params[:media]
+  res = Imgur.upload(params[:media][:tempfile])
+  res.to_json
 end
